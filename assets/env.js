c3bb0de91b7d1f09059ebb61998a17997e017ae2
20d006 <92768424+20d006@users.noreply.github.com>
2022-01-27 20:01:41 +0900

    Update env.js

--- assets/env.js
+++
@@ -1,2 +0,0 @@
-SSID='https://docs.google.com/spreadsheets/d/17sVuFJFDBenYDqv9CTfRzvTJbuSbdpNXZDYWcUmrfzQ/edit#gid=0
-SNAME=okazaki!A2:K1000
---
+++ 財産/env.js
@@ -0,0 +1,2 @@
+SSID=https://docs.google.com/spreadsheets/d/17sVuFJFDBenYDqv9CTfRzvTJbuSbdpNXZDYWcUmrfzQ/edit#gid=0
+SNAME=okazaki!A2:K1000
